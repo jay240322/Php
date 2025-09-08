e6ac707949324475b984a5aef7d4df86e46fb439
jay patel <161129455+jay240322@users.noreply.github.com>
2025-09-08 20:59:29 +0530

    Update 5.php

--- php-3/5.php
+++ php-3/5.php
@@ -1,5 +1,1 @@
 <!-- create a form which takes two input field (product_id,rate) and update button when user click on the update button , the rate of the product_id specified should get updated by 5% of the current rate using a php script -->
-
-
-  <!-- currently i am not able to learn more in this language so this is all
-  i am posting more after some rest of mind -->
