f9ad9e3a025db96032b80fe92ae2b66b248a0a2a
jay patel <patel.jay.brij03@gmail.com>
2025-09-08 22:18:50 +0530

    done

---
+++ practise.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST">
+        enter the raiuds of the circle:
+        <input type="number"name="number">
+        <input type="submit"name="click">
+    </form>
+</body>
+</html>
+<?php
+
+?>
